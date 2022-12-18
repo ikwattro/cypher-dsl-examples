1b2a4030efcc9ca854cff7074cc0d74dff53f5f6
Christophe Willemsen <willemsen.christophe@gmail.com>
2022-12-18 21:29:23 +0100

    moved tests to right place

--- src/test/java/dev/ikwattro/PathTraversalExamplesTest.java
+++ src/test/java/dev/ikwattro/PathTraversalExamplesTest.java
@@ -8,25 +8,57 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PathTraversalExamplesTest extends AbstractCypherDSLTest {
 
     @Test
-    void return_nodes_projection() {
-        var node = Cypher.node("Person").named("person");
-        var statement = Cypher.match(node)
-                .where(node.internalId().in(Cypher.parameter("$ids")))
-                .returning(node
-                        .project("name", node.property("name"), "__labels", node.labels(), "__id", node.internalId())
-                        .as("__node__"))
+    void simple_path_traversal() {
+        var nodeFrom = Cypher.node("Person").named("person");
+        var movieNode = Cypher.anyNode("movie");
+        var actedInRel = nodeFrom.relationshipTo(movieNode, "ACTED_IN", "PRODUCED").named("r");
+        var path = Cypher.path("path").definedBy(actedInRel);
+        var statement = Cypher.match(path)
+                .returning(path)
                 .build();
 
         var cypher = generateQuery(statement);
         String expected = """
-                MATCH (person:`Person`)
-                WHERE id(person) IN $ids
-                RETURN person {
-                  name: person.name,
-                  __labels: labels(person),
-                  __id: id(person)
-                } AS __node__
+                MATCH path = (person:`Person`)-[r:`ACTED_IN`|`PRODUCED`]->(movie)
+                RETURN path
                 """;
         assertThat(cypher).isEqualTo(expected.trim());
+    }
+
+    @Test
+    void shortest_path() {
+        var nodeFrom = Cypher.node("Person").named("person");
+        var movieNode = Cypher.anyNode("movie");
+        var actedInRel = nodeFrom.relationshipTo(movieNode, "ACTED_IN", "PRODUCED").named("r");
+        var path = Cypher.shortestPath("path").definedBy(actedInRel);
+        var statement = Cypher.match(path)
+                .returning(path)
+                .build();
+
+        var cypher = generateQuery(statement);
+        String expected = """
+                MATCH path = shortestPath((person:`Person`)-[r:`ACTED_IN`|`PRODUCED`]->(movie))
+                RETURN path
+                """;
+        assertThat(cypher).isEqualTo(expected.trim());
+    }
+
+    @Test
+    void all_shortest_paths() {
+//        Is it possible yet ?
+//        var nodeFrom = Cypher.node("Person").named("person");
+//        var movieNode = Cypher.anyNode("movie");
+//        var actedInRel = nodeFrom.relationshipTo(movieNode, "ACTED_IN", "PRODUCED").named("r");
+//        var path = Cypher.path("path").definedBy(actedInRel);
+//        var statement = Cypher.match(path)
+//                .returning(path)
+//                .build();
+//
+//        var cypher = generateQuery(statement);
+//        String expected = """
+//                MATCH path = (person:`Person`)-[r:`ACTED_IN`|`PRODUCED`]->(movie)
+//                RETURN path
+//                """;
+//        assertThat(cypher).isEqualTo(expected.trim());
     }
 }
